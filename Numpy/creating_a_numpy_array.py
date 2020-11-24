20c5d6548b212d330436e6a29aedbba4d5d3dca2
hussein wassouf <57813196+hussein343455@users.noreply.github.com>
2020-11-24 20:28:57 +0300

    Add files via upload

---
+++ Numpy/creating_a_numpy_array.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.zeros((3,3))   # Create an array of all zeros
+a = np.ones((2,1))    # Create an array of all ones
+a = np.full((3,3), 7)  # Create a constant array
+a = np.eye(3)         # Create a 2x2 identity matrix
+a = np.random.random((2,2))  # Create an array filled with random values
