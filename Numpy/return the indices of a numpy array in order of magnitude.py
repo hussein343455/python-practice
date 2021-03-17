853c012d20ec30559cc459be02465d5cc7267aef
hussein wassouf <57813196+hussein343455@users.noreply.github.com>
2021-03-17 22:05:31 +0300

    Update return the indices of a numpy array in order of magnitude.py

--- Numpy/return the indices of a numpy array in order of magnitude.py
+++ Numpy/return the indices of a numpy array in order of magnitude.py
@@ -1,8 +1,4 @@
 return the indices of a numpy array in order of magnitude.
 def get_top_magnitude_indices(values):
-    """
-    Helper function for get_top_covariances to get indices by magnitude. 
-    Parameter: values, a list of values as a numpy array of shape (n_values)
-    Returns: numpy array of indices sorted from greatest to least by the magnitudes of their corresponding values
-    """
+   
     return  np.flip(np.argsort(np.abs(values)))
